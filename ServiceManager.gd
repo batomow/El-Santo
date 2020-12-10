028c795e2dd141213c9e06d2f67bf4f79c8c737f
Batomow <carlosa_miranda@outlook.com>
2020-12-09 19:13:18 -0800

    Adds ServiceManager request queueing

--- ServiceManager.gd
+++ ServiceManager.gd
@@ -3,16 +3,25 @@ extends Node
 "Icon made by Freepik from www.flaticon.com"
 class_name ServiceManager #, "res://icons/ServiceManager.svg"
 var services = {}
+var requests = {}
 
 func register(service_name:String, callback:FuncRef): 
-	if services.has(service_name): 
+	if service_name in services: 
 		printerr("service: '", service_name, "' already registered!")
 	else: 
 		services[service_name] = callback
+		if requests.has(service_name): 
+			for req in requests[service_name]: 
+				req.call_func(callback)
+			requests.erase(service_name)
 
-func request(service_name:String): 
-	assert (services.has(service_name))
-	return services[service_name]
 
-func check_for_service(service_name:String):
-	return services.has(service_name)
+func request(service_name:String, callback:FuncRef): 
+	if not service_name in requests: 
+		requests[service_name] = []
+
+	if service_name in services:
+		var service = services[service_name]
+		callback.call_func(service)
+	else: 
+		requests[service_name].push_back(callback)
